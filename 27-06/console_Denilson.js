bb4fd9d9ca6fd2542dba0eac4f7c022933cf7c76
Amílcar Júnior <amilcar.junior@health.cv>
2024-06-27 10:50:51 -0100

    27 06

---
+++ 27-06/console_Denilson.js
@@ -0,0 +1,52 @@
+//allow pasting
+
+let event = new Event('input', {
+    'bubbles': true,
+    'cancelable': true
+});
+
+// Clica no botao desejado (presumivelmente para abrir o formulario)
+document.getElementsByTagName('button')[2].click();
+
+// Preenche o nome
+document.getElementsByTagName("form")[0].querySelectorAll('input')[0].value='DENILSON'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[0].dispatchEvent(event)
+
+// Preenche o sobrenome
+document.getElementsByTagName("form")[0].querySelectorAll('input')[1].value='SEMEDO ROCHA'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[1].dispatchEvent(event)
+
+// Seleciona o genero
+document.getElementsByTagName("form")[0].querySelectorAll('mat-select')[0].click();
+let option2 = Array.from(document.querySelectorAll('.mat-option')).find(opt => opt.innerText === "macho" || opt.innerText === "masculino" || opt.innerText === "male" || opt.innerText === "Macho" || opt.innerText === "Masculino" || opt.innerText === "Male");
+option2.click();
+
+// Preenche a data de nascimento
+document.getElementsByTagName("form")[0].querySelectorAll('input')[2].value='24/07/1998'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[2].dispatchEvent(event)
+
+// Seleciona a nacionalidade
+document.getElementsByTagName("form")[0].querySelectorAll('mat-select')[1].click();
+let option = Array.from(document.querySelectorAll('.mat-option')).find(opt => opt.innerText === "CAPE VERDE");
+option.click();
+
+// Preenche o numero do passaporte
+document.getElementsByTagName("form")[0].querySelectorAll('input')[3].value='PA191870'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[3].dispatchEvent(event)
+
+// Preenche a data de expiracao
+document.getElementsByTagName("form")[0].querySelectorAll('input')[4].value='04/01/2026'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[4].dispatchEvent(event)
+
+// Preenche o codigo do pais
+document.getElementsByTagName("form")[0].querySelectorAll('input')[5].value='238'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[5].dispatchEvent(event)
+
+// Preenche o numero de telefone
+document.getElementsByTagName("form")[0].querySelectorAll('input')[6].value='5803041'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[6].dispatchEvent(event)
+
+// Preenche o email
+document.getElementsByTagName("form")[0].querySelectorAll('input')[7].value='AJ580305541@outlook.com'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[7].dispatchEvent(event)
+
---
+++ 27-06/console_EDMILSON.js
@@ -0,0 +1,62 @@
+//allow pasting
+
+let event = new Event('input', {
+    'bubbles': true,
+    'cancelable': true
+});
+
+// Clica no botao desejado (presumivelmente para abrir o formulario)
+document.getElementsByTagName('button')[2].click();
+
+// Preenche o nome
+document.getElementsByTagName("form")[0].querySelectorAll('input')[0].value='EDMILSON'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[0].dispatchEvent(event)
+
+// Preenche o sobrenome
+document.getElementsByTagName("form")[0].querySelectorAll('input')[1].value='SILVA VAZ'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[1].dispatchEvent(event)
+
+// Seleciona o genero
+document.getElementsByTagName("form")[0].querySelectorAll('mat-select')[0].click();
+let option2 = Array.from(document.querySelectorAll('.mat-option')).find(opt => opt.innerText === "macho" || opt.innerText === "masculino" || opt.innerText === "male" || opt.innerText === "Macho" || opt.innerText === "Masculino" || opt.innerText === "Male");
+option2.click();
+
+// Preenche a data de nascimento
+document.getElementsByTagName("form")[0].querySelectorAll('input')[2].value='09/07/2004'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[2].dispatchEvent(event)
+
+// Seleciona a nacionalidade
+document.getElementsByTagName("form")[0].querySelectorAll('mat-select')[1].click();
+let option = Array.from(document.querySelectorAll('.mat-option')).find(opt => opt.innerText === "CAPE VERDE");
+option.click();
+
+// Preenche o numero do passaporte
+document.getElementsByTagName("form")[0].querySelectorAll('input')[3].value='PA339181'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[3].dispatchEvent(event)
+
+// Preenche a data de expiracao
+document.getElementsByTagName("form")[0].querySelectorAll('input')[4].value='27/04/2028'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[4].dispatchEvent(event)
+
+// Preenche o codigo do pais
+document.getElementsByTagName("form")[0].querySelectorAll('input')[5].value='238'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[5].dispatchEvent(event)
+
+// Preenche o numero de telefone
+document.getElementsByTagName("form")[0].querySelectorAll('input')[6].value='5803032'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[6].dispatchEvent(event)
+
+// Preenche o email
+document.getElementsByTagName("form")[0].querySelectorAll('input')[7].value='AJ580305532@outlook.com'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[7].dispatchEvent(event)
+
+// Clica no botao para submeter o formulario apos 27 segundos
+setTimeout(function() {
+    const botao = document.getElementsByTagName('button')[2];
+    
+    if (botao) {
+        botao.click();
+    } else {
+        console.error('Botao nao encontrado.');
+    }
+}, 27000);
