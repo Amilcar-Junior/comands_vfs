796ddb47e3f58020272736144c70af961b962a3a
Amílcar Júnior <amilcar.junior@health.cv>
2024-07-11 10:58:42 -0100

    news

--- 11-07/console_JOAO.js
+++ 11-07/console_JOAO.js
@@ -9,11 +9,11 @@ let event = new Event('input', {
 document.getElementsByTagName('button')[2].click();
 
 // Preenche o nome
-document.getElementsByTagName("form")[0].querySelectorAll('input')[0].value='JORGE'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[0].value='JOAO'
 document.getElementsByTagName("form")[0].querySelectorAll('input')[0].dispatchEvent(event)
 
 // Preenche o sobrenome
-document.getElementsByTagName("form")[0].querySelectorAll('input')[1].value='TEIXEIRA SILVES FERREIRA'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[1].value='GOMES DA VEIGA'
 document.getElementsByTagName("form")[0].querySelectorAll('input')[1].dispatchEvent(event)
 
 // Seleciona o genero
@@ -22,7 +22,7 @@ let option2 = Array.from(document.querySelectorAll('.mat-option')).find(opt => o
 option2.click();
 
 // Preenche a data de nascimento
-document.getElementsByTagName("form")[0].querySelectorAll('input')[2].value='14/12/1995'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[2].value='02/10/1982'
 document.getElementsByTagName("form")[0].querySelectorAll('input')[2].dispatchEvent(event)
 
 // Seleciona a nacionalidade
@@ -31,11 +31,11 @@ let option = Array.from(document.querySelectorAll('.mat-option')).find(opt => op
 option.click();
 
 // Preenche o numero do passaporte
-document.getElementsByTagName("form")[0].querySelectorAll('input')[3].value='PA342204'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[3].value='PA346555'
 document.getElementsByTagName("form")[0].querySelectorAll('input')[3].dispatchEvent(event)
 
 // Preenche a data de expiracao
-document.getElementsByTagName("form")[0].querySelectorAll('input')[4].value='22/06/2028'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[4].value='04/05/2028'
 document.getElementsByTagName("form")[0].querySelectorAll('input')[4].dispatchEvent(event)
 
 // Preenche o codigo do pais
@@ -43,20 +43,11 @@ document.getElementsByTagName("form")[0].querySelectorAll('input')[5].value='238
 document.getElementsByTagName("form")[0].querySelectorAll('input')[5].dispatchEvent(event)
 
 // Preenche o numero de telefone
-document.getElementsByTagName("form")[0].querySelectorAll('input')[6].value='58030XX'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[6].value='5803066'
 document.getElementsByTagName("form")[0].querySelectorAll('input')[6].dispatchEvent(event)
 
 // Preenche o email
-document.getElementsByTagName("form")[0].querySelectorAll('input')[7].value='AJ5803055XX@outlook.com'
+document.getElementsByTagName("form")[0].querySelectorAll('input')[7].value='AJ580305566@outlook.com'
 document.getElementsByTagName("form")[0].querySelectorAll('input')[7].dispatchEvent(event)
 
-// Clica no botao para submeter o formulario apos 27 segundos
-setTimeout(function() {
-    const botao = document.getElementsByTagName('button')[2];
-    
-    if (botao) {
-        botao.click();
-    } else {
-        console.error('Botao nao encontrado.');
-    }
-}, 27000);
+
